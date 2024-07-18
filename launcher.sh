b58cb15b634f9158fd4c43592f1605f1ddc3b7ed
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-07-18 12:03:26 +0200

    initial version

--- launcher.sh
+++ launcher.sh
@@ -37,15 +37,11 @@ error_exit() {
     echo "$LOG"
     touch $LOG
     echo -e "Crashed!! Email then sleep 15s\n" 
-    echo -e "launcher.sh => i crashed!! Email then sleep 15s\n" >>$LOG
-    echo -e "see err file" | mail -s "$HOST_NAME Crashed!!!!" rudy_vandenberghe@hotmail.com
+    echo -e "launcher.sh => i crashed!!\n" >>$LOG
     sleep 15s   # wait for 15 seconds to allow the job to be killed or restart
     echo -e "Sleep Finished, retrying action!!"
     echo -e "launcher.sh => Sleep Finished, retrying action!!\n" >>$LOG
 }
-# echo -e "sleep 10 seconds to catch /dev/tty1 when it is free"
-sleep 5s
-# OUT="/dev/tty1" # define out
 while true; do
     echo -e "Now Running $HOST_NAME"
     .venv/bin/python -u digital_meter.py
