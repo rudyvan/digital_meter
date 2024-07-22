9676268890370413d10e062b8912072c3f5d3f0b
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-07-22 12:56:19 +0200

    updates docs

--- dm_app/screen.py
+++ dm_app/screen.py
@@ -81,13 +81,13 @@ class Screen:
         for x in self._usage_columns():
             # change the header text to the day of the week if it is a past day
             txt = x if "Day-" not in x else (self.data["cur_time"]-datetime.timedelta(days=int(x.split("-")[1]))).strftime("%A")
-            txt += " (R1)" if "day" in x.lower() else " (R2)" if "night" in x.lower() else ""
             table.add_column(txt, justify="right", style="magenta" if x == "Today" else "green")
 
         for pos, line in enumerate(self._usage_rows()):
             # highlight day or night usage depending on the current rate
             hit = (self.cur_rate == 1 and "Day" in line or self.cur_rate == 2 and "Night" in line)
-            table.add_row(line, *[f"{self.usage[x][pos]:.2f}" for x in self._usage_columns()],
+            line_1st = line + " (R1)" if "day" in line.lower() else " (R2)" if "night" in line.lower() else ""
+            table.add_row(line_1st, *[f"{self.usage[x][pos]:.2f}" for x in self._usage_columns()],
                           style="green" if hit else "blue", end_section=True if "Σ" in line else False)
             if line == "Σ € kWh":
                 # add the 2 quarter peak lines
