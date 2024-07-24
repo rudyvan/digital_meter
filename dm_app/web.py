fc86a2aaa1110969dcaf25b1718130712ae824da
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-07-24 08:56:52 +0200

    move to async

--- dm_app/web.py
+++ dm_app/web.py
@@ -5,6 +5,7 @@ from aiohttp import web
 import websockets
 import socket
 import json
+import datetime
 
 
 app = web.Application()
@@ -24,12 +25,13 @@ class SocketApp:
 
     async def send_ws(self, data, **_) -> (bool, "success"):
         """send data to a socket for a host with ip, port, and path"""
-        self.log_add(f"send_ws {self.ws_ep} {len(data)} bytes")
-        to_snd = data if isinstance(data, str) else json.dumps({"type": "dm", "cmd": "data", "data": data})
         if not self.ws_ep:
             self.log_add(f"send_ws failed as no end point")
             return False
-        self.log_add(f"send_ws {self.ws_ep} {len(to_snd)} bytes")
+        self.log_add(f"send_ws {self.ws_ep} {len(data)=} bytes")
+        encode_JSON = lambda x: self.ts_str(x) if isinstance(x, datetime.datetime) else repr(x)
+        to_snd = data if isinstance(data, str) else json.dumps({"type": "dm", "cmd": "data", "data": data},indent=4, sort_keys=True, default=encode_JSON)
+        self.log_add(f"send_ws {self.ws_ep} {len(to_snd)=} bytes")
         tries = 0
         while True:
             try:
