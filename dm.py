1e57d97ca80fb268ee776556db6afa8acd63b2cf
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-06 20:45:42 +0200

    better structure

--- dm.py
+++ dm.py
@@ -7,14 +7,21 @@ from config import serial_port, socket_info
 
 from dm_app import BusMeter
 
-from app import log_app, pickle_app, SocketApp
+from .app import log_app, pickle_app, SocketApp
 
 from rich.traceback import install
 
 install(width=180, extra_lines=10, show_locals=True)
 
+class DigitalMeter(BusMeter):
+    def __init__(self):
+        self.log_app = log_app
+        self.pickle_app = pickle_app
+        self.socket_app = SocketApp(socket_info)
+        super().__init__(serial_port)
+        self.log_app.console = self.console
+    def run(self):
+        super().run()
+
 if __name__ == '__main__':
-    bm = BusMeter(serial_port)
-    bm.log_app, bm.pickle_app, bm.socket_app = log_app, pickle_app, SocketApp(socket_info)
-    bm.log_app.console = bm.console
-    bm.run()
+    DigitalMeter().run()
