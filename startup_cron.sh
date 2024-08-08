2d9dedc81592915a18e01d596b390742bea47ffd
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-08 11:32:20 +0200

    more logging

--- startup_cron.sh
+++ startup_cron.sh
@@ -10,9 +10,8 @@ else
   APP_DIR=$HOME_DIR/digital_meter
   BIN="/usr/bin"
   PATH=$BIN:$PATH
-  # source .bashrc
-  # source .profile
-  . ${PYENV_ROOT}/versions/3.12.2/bin/activate
+  source .bashrc
+  source .profile
 fi
 cd $APP_DIR
 tmux set-option -g default-shell /bin/bash
