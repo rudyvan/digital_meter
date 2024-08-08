1a0423204b3390417da5ab3647bd823043490b78
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-08 06:44:19 +0200

    module structure changes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 # encoding=utf-8
 from .app import pi
+from .dm_app import BusMeter
--- dm_app/__init__.py
+++ dm_app/__init__.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 #
 from .bus_meter import BusMeter
-from .screens import Screens
-from .usage import Usage
