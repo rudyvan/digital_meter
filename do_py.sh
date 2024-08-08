8fdd0ab141e80005d612a591b264d47c21a18fca
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-08 10:47:52 +0200

    more logging

--- do_py.sh
+++ do_py.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-
-case "$(uname)" in
-    *"Linux"*)
-      HOST_NAME=$(cat /etc/hostname)
-      ;;
-    *"Darwin"*)
-      HOST_NAME=$(hostname | cut -d '.' -f1)
-    ;;
-    *)
-      echo -e "$OS not supported"
-    ;;
-esac
-HOME_DIR=$(echo ~)
-APP_DIR=$HOME_DIR/digital_meter
+if [ -z "$1" ]; then
+  echo "missing <script.py>"
+  exit
+fi
+if [[ $(uname) == *"Darwin"* ]]; then
+  APP_DIR=$(echo ~)/MyApps/digital_meter
+else
+  APP_DIR=$(echo ~)/digital_meter
+fi
 cd $APP_DIR
-sudo /bin/bash -c "source .venv/bin/activate"
-source .venv/bin/activate
+#sudo /bin/bash -c "source .venv/bin/activate"
+#source .venv/bin/activate
 while true; do
     python -u $1
     if [ $? -eq 1 ]; then
-      echo -e "Crashed!! Email then sleep 15s\n"
+      echo -e "Crashed!! -> sleep 15s\n"
       echo -e "do_py.sh => i crashed!!\n"
-      sleep 15s   # wait for 15 seconds to allow the job to be killed or restart
+      sleep 15
       echo -e "Sleep Finished, retrying action!!"
     fi
 done
