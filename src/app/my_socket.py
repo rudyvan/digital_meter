b61d5168f7aaeeae8f03c6473552b5190db5caab
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-09 16:21:50 +0200

    ths transfer

--- src/app/my_socket.py
+++ src/app/my_socket.py
@@ -53,7 +53,7 @@ class SocketApp:
         async for websocket in websockets.connect(end_p):
             try:
                 data = await self._send_queues[ip].get()
-                data_str = str(data).replace('\n', '')
+                data_str = str(data).replace('\n', '').replace("  ", "")
                 self.log_app.add(f"Websocket Send to {end_p} --> {data_str}", tpe="debug")
                 await websocket.send(data)
             except websockets.ConnectionClosed:
