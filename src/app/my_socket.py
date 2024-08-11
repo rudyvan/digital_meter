b3488cb556de456ca63500004b528e40c5b16138
Rudy Vandenberghe <rudy_vandenberghe@hotmail.com>
2024-08-11 07:59:30 +0200

    update in socket

--- src/app/my_socket.py
+++ src/app/my_socket.py
@@ -101,7 +101,7 @@ class SocketApp:
                 data_dct["val"] = self.get_val(getattr(self.DM_selfie, obdis_th, 0.0))
                 return await self.send_ws(data_dct, ip)
             case "cum" | "usage":  # ask for a cum or usage
-                self.log_app.add(f"Things_sync ignored Forensics request: {data}")
+                self.log_app.add(f"Things_sync ignored Forensics request: {data}", tpe="debug")
             case _:
                 return
 
